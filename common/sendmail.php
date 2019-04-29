46fe3fc38baa34ba83c51f7762db032706a66dbb
Christophe Parisse <christopheparisse@gmail.com>
2019-04-29 15:12:33 +0200

    version 0.1.1

--- common/sendmail.php
+++ common/sendmail.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 /*
 $validator = $pp->getValidator();
 $validator->fields(['nom','prénom','email'])->areRequired()->maxLength(50);
@@ -23,15 +19,18 @@ $validator->field('genre_de_discours')->maxLength(6000);
 $pp->attachFiles(['file']);
 */
 
-/*
-$validator->fields(['title','name','email'])->areRequired()->maxLength(50);
-$validator->field('title')->maxLength(6000);
-$validator->field('name')->maxLength(6000);
-$validator->field('email')->isEmail();
-$validator->field('message')->maxLength(6000);
-*/
+$title=$_POST['title'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['text'];
+$headers = 'From: contactsvalange@gmail.com' . "\r\n" .
+    'Reply-To: contactvalange@gmail.com' . "\r\n" ;
+$headers .= "MIME-Version: 1.0\r\n" . "Content-Type: text/html; charset=UTF-8\r\n";
 
-// $pp->sendEmailTo("contactvalange@gmail.com"); // ← Your email here
-// echo $pp->process($_POST);
-echo 1
+#echo "<h1>", $title, "-X-", $name, "-X-", $email, "-X-", $message, "</h1>";
+#file_put_contents('logvalange.xml', "<msg>\r\n<title>" . $title . "</title>\r\n<name>" . $name . "</name>\r\n<email>" . $email . "</email>\r\n<text>" . $message . "</text></msg>\r\n\r\n", FILE_APPEND);
+mail($email, $title, "<p>Merci de nous avoir contacté.<br/>L'équipe de Valange<br/><a href='http://ct3.ortolang.fr/valange/'>ct3.ortolang.fr/valange/</a><p>", $headers);
+#mail("contactsvalange@gmail.com", "selfcontact", $email . "\r\n" . $title . "\r\n"  . $name . "\r\n-----\r\n" . $message, $headers);
+mail("contactsvalange@gmail.com", "selfcontact", "<pre><msg>\r\n<title>" . $title . "</title>\r\n<name>" . $name . "</name>\r\n<email>" . $email . "</email>\r\n<text>" . $message . "</text></msg>\r\n</pre>\r\n", $headers);
+echo mail("contactvalange@gmail.com", "contact site valange", "Email: " . $email . "\r\n" . "Titre: " . $title . "\r\n" . "Nom: " . $name . "\r\n" . "Message:" . $message, $headers);
 ?>
