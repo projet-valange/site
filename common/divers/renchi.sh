cbd61bb624389d561f1c338f92226921f58d5cdc
Christophe Parisse <christopheparisse@gmail.com>
2019-06-11 09:49:11 +0200

    version 0.2.0

---
+++ common/divers/renchi.sh
@@ -0,0 +1,2 @@
+sed -e "s/<child>Antoine</<child>Antoine M</" -i .tmp data/doc/*/metadata.xml
+sed -e "s/<child>Antoine Bi</<child>Antoine B</" -i .tmp data/doc/*/metadata.xml
---
+++ data/json/categories.js
@@ -0,0 +1,37 @@
+var activitiesCategories = {
+    "Poupée peluche figurines": "Jeux",
+    "Jeu de construction": "Jeux",
+    "Jeu en extérieur": "Jeux",
+    "Jeu de société": "Jeux",
+    "Jeu symbolique": "Jeux",
+    "Jeu technologique": "Jeux",
+    "Jeu autre": "Jeux",
+    "Repas": "Vie Quotidienne",
+    "Soin": "Vie Quotidienne",
+    "Devoirs": "Apprentissages",
+    "Politesse": "Apprentissages",
+    "Rangement": "Apprentissages",
+    "Téléphone": "Apprentissages",
+    "Dessin": "Créativité",
+    "Livre": "Créativité",
+    "Musique": "Créativité",
+};
+
+var activitiesPictures = {
+    "Jeux": "johnny_automatic_boy_playing_with_toy_truck.svg",
+    "Vie Quotidienne": "Anonymous_Architetto_--_Come_si_dispongono_le_posate.svg",
+    "Apprentissages": "telefono_email_frolland_01.svg",
+    "Créativité": "AJ_Books.svg"
+};
+
+var childrenPictures = {
+    "Anaé": "anae.jpg",
+    "Antoine (fr)": "antoine.jpg",
+    "Antoine (it/fr)": "antoine-it-fr.png",
+    "Gustavo": "gustavo7.jpg",
+    "Julie": "julie.jpg",
+    "Madeleine": "madeleine.jpg",
+    "Philippine": "philippine.jpg",
+    "Théophile": "theophile.jpg",
+    "Ulysse": "default.jpg",
+};
