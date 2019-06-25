546578ef63076daeba2be296b3203fadf8d8f76e
Christophe Parisse <christopheparisse@gmail.com>
2019-06-25 14:55:48 +0200

    version 0.2.0

--- send.sh
+++
@@ -1,2 +0,0 @@
-# rsync -auv * parisse@ct3.ortolang.fr:/applis/valange/
-rsync -auv --exclude="*.mov" --exclude="*.mpg" --exclude="old/" --exclude="node_modules/" --exclude="depository/" * parisse@ct3.ortolang.fr:/applis/valange/
