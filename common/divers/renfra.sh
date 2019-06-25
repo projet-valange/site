c4b47fd563e39d69d8da0f693ea906be6fc67f8f
Christophe Parisse <christopheparisse@gmail.com>
2019-06-25 14:54:52 +0200

    version 0.2.0

---
+++ common/divers/renfra.sh
@@ -0,0 +1,3 @@
+sed -e "s/>fra, ita</>fre, ita</" -i .tmp data/doc/*/metadata.xml
+sed -e "s/>ita, fra</>ita, fre</" -i .tmp data/doc/*/metadata.xml
+sed -e "s/>fra</>fre</" -i .tmp data/doc/*/metadata.xml
